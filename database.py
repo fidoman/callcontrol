36a744c9760e5cd88e3685ab2919f7e5b9f7f8f9
Sergey Dorofeev <sergey@fidoman.ru>
2017-07-06 17:53:28 +0200

    add file

---
+++ database.py
@@ -0,0 +1,20 @@
+import json
+
+""" 
+Module for database access
+1. Updates:
+     store in local queue
+     upload as soon as possible
+2. Queries:
+     online
+
+Keep connection, reestablish if needed
+"""     
+
+def save_record(r):
+  
+
+
+class CallsDB():
+  def __init__(self):
+    self.conndata=
