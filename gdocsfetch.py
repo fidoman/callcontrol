84a466b7ae6f56a10a9a2b44123d358d36799b81
Sergey <sergey@fidoman.ru>
2017-07-07 17:14:23 +0300

    add file

---
+++ gdocsfetch.py
@@ -0,0 +1,100 @@
+import httplib2
+import os
+import json
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def fetch_doc(doc_id):
+    """Shows basic usage of the Sheets API.
+
+    Creates a Sheets API service object and prints the names and majors of
+    students in a sample spreadsheet:
+    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+#    spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+    spreadsheetId = doc_id
+#    rangeName = 'C2:C59'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range='A:Z').execute() #, range=rangeName
+    values = result.get('values', [])
+
+    return values
+
+
+def fetch_all():
+  doc_list=json.load(open('gdocs.json'))
+  for what, doc in doc_list.items():
+    print(what)
+    data = fetch_doc(doc)
+    print(data)
+   
+
+#    if not values:
+#        print('No data found.')
+#    else:
+#        out = []
+#        for row in values:
+#          if row:
+#            # Print columns A and E, which correspond to indices 0 and 4.
+#            if len(row)>=5:
+#              pass #print(row[0], row[2], row[4:])
+#              out.append((row[0], row[2], row[4:]))
+#            else:
+#              print(row)
+#        import json
+#        json.dump(out, open("shops.json", "w"))
+
+if __name__ == '__main__':
+    fetch_all()
