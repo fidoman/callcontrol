9b8987106f8a2681bdc9dd3fba88c2ff742a86f1
Sergey <sergey@fidoman.ru>
2017-08-10 21:38:26 +0300

    configuraton responder

--- data.py
+++ data.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 import cgi
-import cgitb
+#import cgitb
 import json
 import sys
 import postgresql
 import traceback
+import socket
+import configparser
 
-cgitb.enable(display=0, logdir="/var/log/ccdata")
+#cgitb.enable(display=0, logdir="/var/log/ccdata")
 
 form = cgi.FieldStorage()
 
@@ -17,8 +19,8 @@ dbconn = json.load(open("database.json"))
 db = postgresql.open(**dbconn)
 
 def check_user(ext, pw):
-  allowed = True
-  for (c,) in db.prepare("select count(*) from extensions where ext_n=$1 and ext_pw=$2")(ext, pw):
+  allowed = False
+  for (c,) in db.prepare("select * from check_ext_pw($1, $2)")(ext, pw):
     if c==1:
       allowed = True
   if allowed:
@@ -49,12 +51,24 @@ try:
     ext = form.getvalue("ext")
     pw = form.getvalue("pw")
     check_user(ext, pw)
-    out["name"] = ''
+    out["name"] = socket.gethostname()
+    out["manager_user"] = "callcontrol"
+
+    conf = configparser.ConfigParser()
+    conf.read("/etc/asterisk/manager.d/callcontrol.conf")
+    out["manager_pw"] = conf['callcontrol']['secret']
+
+    conf = configparser.ConfigParser()
+    conf.read("/etc/asterisk/manager.conf")
+    out["manager_host"] = socket.gethostname()
+    out["manager_port"] = conf['general']['port']
+
+    
 
 except Exception as e:
   print("Content-type: text/plain\n")
   print("error", type(e)) #, e.code, e.creator)
-#  print(traceback.format_exc())
+  print(traceback.format_exc())
   exit()
 
 print("Content-type: application/json\n")
