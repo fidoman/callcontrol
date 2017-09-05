819690533f1d6f7280d2e6dc52aa75ce59a73063
Sergey Dorofeev <sdorofeev@fidoman.ru>
2017-09-05 16:55:45 +0300

    combined installer

--- install/ccinstall.cmd
+++ install/ccinstall.cmd
@@ -1,60 +1,69 @@
 @echo off
 
 if .%1 == .elevated goto elevated
-if .%1 == .py_ok goto py_ok
-if .%1 == .bypass_git goto bypass_git
 
-set ME=%~dp0ccinstall.cmd
+set ME=%0
 echo restart %ME%...
-powershell -Command "Start-Process %ME% elevated -Verb RunAs"
-if errorlevel 1 goto fail
+powershell -Command "Start-Process ""%ME%"" elevated -Verb RunAs"
 exit 0
 
 :elevated
 echo elevated
 
+
+set MUSTQUIT=0
+rem set to 1 if installed python or git and must refresh PATH
+
+
 python -V
-if not errorlevel 1 goto py_ok
+if not errorlevel 1 goto python_ok
+
+if exist "%~dp0python-3.6.2.exe" goto python_downloaded
+echo download python
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"https://www.python.org/ftp/python/3.6.2/python-3.6.2.exe\", \"%~dp0python-3.6.2.exe\")"
-%~dp0python-3.6.2.exe /passive InstallAllUsers=1 PrependPath=1
-if errorlevel 1 goto fail
-rem  CompileAll=1
+:python_downloaded
+echo install python
 
-set ME=%~dp0ccinstall.cmd
-echo restart %ME%...
-powershell -Command "Start-Process %ME% py_ok -UseNewEnvironment"
-if errorlevel 1 goto fail
-exit 0
+set MUSTQUIT=1
+"%~dp0python-3.6.2.exe" /passive InstallAllUsers=1 PrependPath=1 CompileAll=1
 
-:py_ok
+:python_ok
+
+
+if exist "%~dp0MicroSIP-3.15.7.exe" goto microsip_downloaded
+echo download microsip
+rem powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"https://www.microsip.org/downloads/?file=MicroSIP-3.15.7.exe\", \"%~dp0MicroSIP-3.15.7.exe\")"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"http://sergey.fidoman.ru/files/MicroSIP-3.15.7.exe\", \"%~dp0MicroSIP-3.15.7.exe\")"
+:microsip_downloaded
+rem echo install MicroSIP
+rem "%~dp0MicroSIP-3.15.7.exe" /S
+
+
+git --version
+if not errorlevel 1 goto git_ok
+
+if exist "%~dp0Git-2.14.0.2-32-bit.exe" goto git_downloaded
+echo download git
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"https://github.com/git-for-windows/git/releases/download/v2.14.0.windows.2/Git-2.14.0.2-32-bit.exe\", \"%~dp0Git-2.14.0.2-32-bit.exe\")"
+:git_downloaded
+echo install git
+set MUSTQUIT=1
+"%~dp0Git-2.14.0.2-32-bit.exe" /silent
+
+:git_ok
+
+if not %MUSTQUIT% == 1 goto part1ok
+
+echo Prerequisites have been installed, please run again
+pause 
+exit
+
+:part1ok
+echo Part 1 OK
 
 echo installing python modules
-pip install asterisk-ami
+pip install asterisk-ami persist-queue
 if errorlevel 1 goto fail
-
-echo run configurator
-python %~dp0microsipconf.py
-if errorlevel 1 goto fail
-
-if exist %~dp0MicroSIP-3.15.7.exe goto bypass_microsip
-echo install MicroSIP
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"https://www.microsip.org/downloads/?file=MicroSIP-3.15.7.exe\", \"%~dp0MicroSIP-3.15.7.exe\")"
-%~dp0MicroSIP-3.15.7.exe /S
-if errorlevel 1 goto fail
-:bypass_microsip
-
-if exist %~dp0Git-2.14.0.2-32-bit.exe goto bypass_git
-echo install git
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile(\"https://github.com/git-for-windows/git/releases/download/v2.14.0.windows.2/Git-2.14.0.2-32-bit.exe\", \"%~dp0Git-2.14.0.2-32-bit.exe\")"
-%~dp0Git-2.14.0.2-32-bit.exe /silent
-if errorlevel 1 goto fail
-set ME=%~dp0ccinstall.cmd
-echo restart %ME%...
-powershell -Command "Start-Process %ME% bypass_git -UseNewEnvironment"
-if errorlevel 1 goto fail
-exit 0
-
-:bypass_git
 
 echo install callcontrol
 c:
@@ -70,6 +79,18 @@ git pull
 if errorlevel 1 goto fail
 
 :finish
+echo run configurator
+python c:\callcontrol\install\microsipconf.py
+if errorlevel 1 goto fail
+
+echo install MicroSIP
+"%~dp0MicroSIP-3.15.7.exe" /S
+if errorlevel 1 goto fail
+
+echo create shortcuts
+c:\callcontrol\install\shortcuts.vbs
+
+echo Part 2 OK
 pause
 exit 0
 
