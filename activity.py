366700f8e558538c14da34a936fab2cdb1a0f274
Sergey Dorofeev <sergey@fidoman.ru>
2018-06-13 19:19:31 +0300

    activity monitor

---
+++ activity.py
@@ -0,0 +1,12 @@
+# asterisk activity monitor
+#  - users online time
+#  - users queue active time
+#  - users call/speak time
+# events relay for subscribed extensions:
+#  - extension status
+#  - external channel status watching for calls came to the extension
+#    notifications about call that gone to other extension/parked/returned
+
+# 1) connect to asterisk AMI
+# 2) accept connection from operator software
+#    minimum delay on requests
